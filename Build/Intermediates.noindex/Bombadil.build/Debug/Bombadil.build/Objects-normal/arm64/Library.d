ee0f7c83dd4f7abb1cc9a68dcfa45dd3cf2e6851
kalebburnham <burnham.kaleb@gmail.com>
2022-07-12 23:13:43 -0500

    Added .gitignore; removed tracking of build files

--- Build/Intermediates.noindex/Bombadil.build/Debug/Bombadil.build/Objects-normal/arm64/Library.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/kalebburnham/Workspaces/Bombadil/Bombadil/Library.cpp \
-  /Users/kalebburnham/Workspaces/Bombadil/Bombadil/Library.hpp
--- Build/Intermediates.noindex/Bombadil.build/Debug/Bombadil.build/Objects-normal/arm64/arbitrage.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/kalebburnham/Workspaces/Bombadil/Bombadil/arbitrage.cpp \
-  /Users/kalebburnham/Workspaces/Bombadil/Bombadil/arbitrage.hpp \
-  /Users/kalebburnham/Workspaces/Bombadil/Bombadil/Library.hpp
